a0c3fd4524f756e75aa87c0afa5dbae88a77d92f
Zulfa Basmallah Agustyn <zulfaba15@gmail.com>
2023-05-03 21:08:30 +0700

    Membuat website sederhana dengan HTML, CSS, dan PHP.

---
+++ result.php
@@ -0,0 +1,7 @@
+<?php
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    echo "<div id='result'>Terima kasih, $name, email Anda ($email) telah terkirim.</div>";
+}
+?>
